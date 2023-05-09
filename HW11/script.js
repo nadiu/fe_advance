ec0a56987256db0967374da4b6af7a2ec4b11ebe
Nadya Klimchuk <nadya.klimchuk@gmail.com>
2023-05-09 19:28:43 +0300

    hw11

---
+++ HW11/script.js
@@ -0,0 +1,22 @@
+function getRandomChinese(length){
+    return new Promise(resolve => {
+        let result = '';
+        let i = 0;
+        while (i < length) {
+            i++;
+            setTimeout(() => {
+                let constSign = Date.now().toString().slice(-5)
+                let numb = String.fromCharCode(constSign) 
+                result += numb
+                if (result.length === length) 
+                    resolve (result)
+                }, i * 50)
+            }
+           
+        }).then((result) => console.log(result));
+    }
+getRandomChinese(4)
+
+
+
+
